e0916217b6d583a9ec7f8296a2572a07bb53a171
viniciusidacruz <viniciusidacruz@hotmail.com>
2025-02-12 14:55:02 -0300

    feat: :sparkles: Create a new files for shared application
    Create a new files for shared application

---
+++ src/modules/@shared/domain/value-objects/id/id.value-object.ts
@@ -0,0 +1,15 @@
+import { v4 as uuid } from "uuid";
+
+import { ValueObject } from "../value-object.interface";
+
+export class Id implements ValueObject {
+	private _id: string;
+
+	constructor(id?: string) {
+    this._id = id || uuid();
+  }
+
+	get id(): string {
+    return this._id;
+  }
+}
---
+++ src/modules/@shared/domain/value-objects/index.ts
@@ -0,0 +1,1 @@
+export * from "./id/id.value-object";
