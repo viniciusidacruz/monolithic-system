9caac18312b33923cf2750d7be638230322ca9b7
viniciusidacruz <viniciusidacruz@hotmail.com>
2025-03-13 11:58:57 -0300

    feat: :sparkles: Implement method in place order usecase for validate products
    Implement method in place order usecase for validate products

--- src/modules/checkout/usecases/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecases/place-order/place-order.usecase.spec.ts
@@ -3,14 +3,14 @@ import { PlaceOrderUseCase } from "./place-order.usecase";
 import { PlaceOrderUseCaseInputDTO } from "./place-order.usecase.dto";
 
 describe("Place order use case unit test", () => {
+	//@ts-expect-error
+	const placeOrderUseCase = new PlaceOrderUseCase();
+
 	describe("Execute method", () => {
 		it("Should throw an error when client not found", async () => {
 			const mockClientFacade = {
 				findClient: vi.fn().mockResolvedValue(null),
 			};
-
-			//@ts-expect-error
-			const placeOrderUseCase = new PlaceOrderUseCase();
 
 			//@ts-expect-error
 			placeOrderUseCase["_clientFacade"] = mockClientFacade;
@@ -20,6 +20,71 @@ describe("Place order use case unit test", () => {
 			await expect(placeOrderUseCase.execute(input)).rejects.toThrow(
 				"Client not found."
 			);
+		});
+	});
+
+	describe("validateProducts method", () => {
+		it("Should throw error if no products are selected", async () => {
+			const mockClientFacade = {
+				findClient: vi.fn().mockResolvedValue(true),
+			};
+
+			//@ts-expect-error
+			placeOrderUseCase["_clientFacade"] = mockClientFacade;
+
+			const input: PlaceOrderUseCaseInputDTO = { clientId: "0", products: [] };
+
+			await expect(placeOrderUseCase.execute(input)).rejects.toThrow(
+				"No products selected"
+			);
+		});
+
+		it("Should throw an error when product is out of stock", async () => {
+			const mockClientFacade = {
+				findClient: vi.fn().mockResolvedValue(true),
+			};
+
+			//@ts-expect-error
+			placeOrderUseCase["_clientFacade"] = mockClientFacade;
+
+			const mockProductFacade = {
+				checkStock: vi.fn().mockImplementation(async ({ productId }) => ({
+					productId,
+					stock: productId === "1" ? 0 : 1,
+				})),
+			};
+
+			//@ts-expect-error
+			placeOrderUseCase["_productFacade"] = mockProductFacade;
+
+			let input: PlaceOrderUseCaseInputDTO = {
+				clientId: "0",
+				products: [{ productId: "1" }],
+			};
+
+			await expect(
+				placeOrderUseCase["validateProducts"](input)
+			).rejects.toThrow("Product 1 is not available in stock.");
+
+			input = {
+				clientId: "0",
+				products: [{ productId: "0" }, { productId: "1" }],
+			};
+
+			await expect(
+				placeOrderUseCase["validateProducts"](input)
+			).rejects.toThrow("Product 1 is not available in stock.");
+			expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(3);
+
+			input = {
+				clientId: "0",
+				products: [{ productId: "0" }, { productId: "1" }, { productId: "2" }],
+			};
+
+			await expect(
+				placeOrderUseCase["validateProducts"](input)
+			).rejects.toThrow("Product 1 is not available in stock.");
+			expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(5);
 		});
 	});
 });
--- src/modules/checkout/usecases/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecases/place-order/place-order.usecase.ts
@@ -1,5 +1,6 @@
 import { UseCaseInterface } from "../../../@shared/usecases/use-case.interface";
 import { ClientAdmFacadeInterface } from "../../../client-adm/facade/client-adm.facade.interface";
+import { ProductAdmFacadeInterface } from "../../../product-adm/facade/product-adm.facade.interface";
 import {
 	PlaceOrderUseCaseInputDTO,
 	PlaceOrderUseCaseOutputDTO,
@@ -9,7 +10,10 @@ export class PlaceOrderUseCase
 	implements
 		UseCaseInterface<PlaceOrderUseCaseInputDTO, PlaceOrderUseCaseOutputDTO>
 {
-	constructor(private readonly _clientFacade: ClientAdmFacadeInterface) {}
+	constructor(
+		private readonly _clientFacade: ClientAdmFacadeInterface,
+		private readonly _productFacade: ProductAdmFacadeInterface
+	) {}
 
 	async execute(
 		input: PlaceOrderUseCaseInputDTO
@@ -22,6 +26,8 @@ export class PlaceOrderUseCase
 			throw new Error(`Client not found.`);
 		}
 
+		await this.validateProducts(input);
+
 		return {
 			id: "",
 			invoiceId: "",
@@ -29,5 +35,25 @@ export class PlaceOrderUseCase
 			products: [],
 			total: 0,
 		};
+	}
+
+	private async validateProducts(
+		input: PlaceOrderUseCaseInputDTO
+	): Promise<void> {
+		if (input.products.length === 0) {
+			throw new Error("No products selected");
+		}
+
+		for (const p of input.products) {
+			const product = await this._productFacade.checkStock({
+				productId: p.productId,
+			});
+
+			if (product.stock <= 0) {
+				throw new Error(
+					`Product ${product.productId} is not available in stock.`
+				);
+			}
+		}
 	}
 }
